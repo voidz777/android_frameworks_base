f8d10a79b2f1923388fd6a31132888a1abc6101c
Sam Mortimer <sam@mortimer.me.uk>
2014-11-16 20:03:53 -0800

    [1/2] frameworks/base: Battery and Notification Lights
    PS2: *) Add back a few missing pieces
         *) Do not clear light state in onPanelRevealed(),
            update lights on ACTION_SCREEN_ON instead
    
    PS3: *) Wrap long lines
         *) Disable config_multiColorNotificationLed by default
         *) Revert config_defaultNotificationLedOff
            from 1000 back to AOSP default of 2000
    
    PS4: *) Clear lights in onPanelRevealed() if
            keyguard is not active.
    
    PS5: Coding style tuning
    
    Pulled together from:
    
    Author: DvTonder <david.vantonder@gmail.com>
    Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Date:   Mon Nov 4 00:57:51 2013 +0000
    Framework: Port CM9 features to CM10
    Change-Id: Ibd63116df90b06f6ce6adb8a0343059bbb999bfb
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Sun Dec 8 15:24:41 2013 +0700
    BatteryService: fix FC on boot until battery stat is present
    updateLightsLocked() can be called from CM's added SettingsObserver
    when battery stat is not present, causing an FC and a loop until
    battery stat is present.
    Change-Id: Ic4438fe50e98f1aa05ae1d0d26240bf9410fd92f
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Tue Dec 31 16:22:05 2013 -0800
    [2/2] Framework: instant led test
    Adds support a new notification extra boolean
    EXTRA_FORCE_SHOW_LIGHTS.  Used by settings
    notification light picker to force lights on
    when the screen is on.
    Change-Id: If0a42d32b28fe8c02ef5f7dd148db7eb478fac17
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Wed Apr 24 11:09:55 2013 +0200
    Allow creating custom dialogs in DialogPreference.
    This allows creating non-AlertDialogs while still using the click and
    state handling of DialogPreference.
    Change-Id: I54fd7d6d6b9f6a49fef1ae95e0178838b3edfa56
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Mon Aug 18 04:56:28 2014 +0300
    Add support for single color notification LED (1/2)
    Change-Id: I367af77036da9e87c6dd0df552ce4c56d945a44d
    
    Change-Id: I564fdb6ca6c5bbcf890afa4b9a2f9fa7bf8b9604

--- core/java/com/android/internal/logging/MetricsConstants.java
+++ core/java/com/android/internal/logging/MetricsConstants.java
@@ -274,6 +274,10 @@ public interface MetricsConstants {
     // Power menu
     public static final int POWER_MENU_SETTINGS = 244;
 
+    // Notifcation and battery settings
+    public static final int NOTIFICATION_LIGHT_SETTINGS = 245;
+    public static final int BATTERY_LIGHT_SETTINGS = 246;
+
     // These constants must match those in the analytic pipeline, do not edit.
     // Add temporary values to the top of MetricsLogger instead.
 
