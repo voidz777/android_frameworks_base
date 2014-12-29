d5bb58bbf7a491f7e3a13148d067729f297771af
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-12-29 07:12:32 +0000

    Revert "Hang off call by power button while screen is on"
    This reverts commit 20accbd736040e66318ba47e50ce1567f999827b.

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4812,7 +4812,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             telecomManager.silenceRinger();
                         } else if ((mIncallPowerBehavior
                                 & Settings.Secure.INCALL_POWER_BUTTON_BEHAVIOR_HANGUP) != 0
-                                && telecomManager.isInCall() && interactive && isScreenOn()) {
+                                && telecomManager.isInCall() && interactive) {
                             // Otherwise, if "Power button ends call" is enabled,
                             // the Power button will hang up any current active call.
                             hungUp = telecomManager.endCall();
