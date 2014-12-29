7f7d458fa2809f66f404e2e296b1147772ddc54a
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3340,6 +3340,24 @@ public final class Settings {
          */
         public static final String HEADS_UP_SHOW_STATUS_BUTTON = "heads_up_show_status_button";
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",  
+                "task_close",  
+                "task_to_front",
+                "task_to_back",  
+                "wallpaper_open",  
+                "wallpaper_close",  
+                "wallpaper_intra_open",  
+                "wallpaper_intra_close",  
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
