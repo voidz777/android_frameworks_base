4f44c3a6bd7190d6bff6b5b1501dbc2a20f17671
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-31 21:49:21 +0000

    QS: add Screenshot tile (1/2)
    Change-Id: I5cefcd20391ffe8fb1bc40e92c522500e5315de7

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -40,6 +40,7 @@ public class QSConstants {
     public static final String TILE_LTE = "lte";
     public static final String TILE_VISUALIZER = "visualizer";
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
+    public static final String TILE_SCREENSHOT = "screenshot";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -70,5 +71,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_LTE);
         TILES_AVAILABLE.add(TILE_VISUALIZER);
         TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
+        TILES_AVAILABLE.add(TILE_SCREENSHOT);
     }
 }
