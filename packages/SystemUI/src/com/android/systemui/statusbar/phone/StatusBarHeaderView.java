e6ba90db50927136290b4c11f171a794b02fec7a
BigBrother1984 <carlosavignano@aospa.co>
2015-03-02 09:51:22 +0000

    Base: Improve HeadsUp [1/2]
    This reworks our original commit https://github.com/Euphoria-OS/android_frameworks_base/commit/18dc0dd88ca10d699efc29aca4433543a361988b
    to include Googles Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED
    
    AOSPA commit can be found here https://github.com/AOSPA/android_frameworks_base/commit/b62eeedf6f99760f88ee62d6c163c92092f4294f
    
    * Add multiuser support, provide a system setting to override heads up
      showing. User can disable heads up within this one so we don't need to
      mess with Settings.Global heads up dependencies and break Google wide
      implementations like priority filtering etc.
    * Add FLAG_WATCH_OUTSIDE_TOUCH, since we manage outside touch events.
    * Hide heads up after 1.0 second on touch outside and/or on home button
      pressing (more specifically when animateCollapsePanels() is called).
    
    Change-Id: I0449a681aca145fd083859381759ac0e41f2f012

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -680,19 +680,19 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
     private void startHeadsUpActivity() {
         Settings.System.putIntForUser(mContext.getContentResolver(),
-            Settings.System.HEADS_UP_NOTIFICATION,
-        mHeadsUpEnabled ? 0 : 1, UserHandle.USER_CURRENT);
+            Settings.System.HEADS_UP_USER_ENABLED,
+        getUserHeadsUpState() ? 0 : 1, UserHandle.USER_CURRENT);
         mActivityStarter.startAction(true /* dismissShade */);
 
         /* show a toast */
         String enabled = mContext.getString(R.string.heads_up_enabled);
         String disabled = mContext.getString(R.string.heads_up_disabled);
         int duration = Toast.LENGTH_SHORT;
-        if (mHeadsUpEnabled) {
-            Toast toast = Toast.makeText(mContext, disabled, duration);
+        if (getUserHeadsUpState()) {
+            Toast toast = Toast.makeText(mContext, enabled, duration);
             toast.show();
         } else {
-            Toast toast = Toast.makeText(mContext, enabled, duration);
+            Toast toast = Toast.makeText(mContext, disabled, duration);
             toast.show();
         }
     }
@@ -1029,8 +1029,6 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
                     Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.HEADS_UP_NOTIFICATION), false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HEADS_UP_SHOW_STATUS_BUTTON), false, this, UserHandle.USER_ALL);
             update();
         }
@@ -1065,8 +1063,6 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
             mShowBatteryTextExpanded = showExpandedBatteryPercentage;
             mShowWeather = Settings.System.getIntForUser(
                     resolver, Settings.System.STATUS_BAR_SHOW_WEATHER, 0, currentUserId) == 1;
-            mHeadsUpEnabled = Settings.System.getIntForUser(
-                    resolver, Settings.System.HEADS_UP_NOTIFICATION, 1, currentUserId) == 1;
             mShowHeadsUpButton = Settings.System.getIntForUser(
                     resolver, Settings.System.HEADS_UP_SHOW_STATUS_BUTTON, 0, currentUserId) == 1;
             updateVisibilities();
@@ -1075,10 +1071,17 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         }
     }
 
+    private boolean getUserHeadsUpState() {
+         return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HEADS_UP_USER_ENABLED,
+                Settings.System.HEADS_UP_USER_ON,
+                UserHandle.USER_CURRENT) != 0;
+    }
+
     private void updateHeadsUpButton() {
         Drawable d = getResources().getDrawable(R.drawable.ic_heads_up);
         ImageView image = (ImageView)findViewById(R.id.heads_up_button);
-        if (mHeadsUpEnabled) {
+        if (getUserHeadsUpState()) {
             image.setImageDrawable(d);
         } else {
             image.setImageDrawable(
