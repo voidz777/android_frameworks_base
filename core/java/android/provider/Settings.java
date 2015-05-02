b35f399c54e45a3f9360d916199a13a2fdb42ac3
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-05-02 04:01:16 +0100

    Base: option to disable torch when the screen turns off (1/2)
    * default delay is set to 10 seconds which can be changed
    
    Change-Id: Ieb7dc96736762b97cc5811cd533208877229a28b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3601,6 +3601,18 @@ public final class Settings {
                 "volume_keys_control_ring_stream";
 
         /**
+         * Whether to disable the torch when the screen turns off
+         * @hide
+         */
+        public static final String DISABLE_TORCH_ON_SCREEN_OFF = "disable_torch_on_screen_off";
+
+        /**
+         * Delay to disable the torch when the screen turns off
+         * @hide
+         */
+        public static final String DISABLE_TORCH_ON_SCREEN_OFF_DELAY = "disable_torch_on_screen_off_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
