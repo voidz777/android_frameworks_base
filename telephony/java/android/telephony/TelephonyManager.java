eddee6f04f21a17ebf2e125e71621a1111d7e4b6
Danny Baumann <dannybaumann@web.de>
2015-05-07 13:23:41 +0200

    Remove some useless debug spam.
    Change-Id: I688c15dc179a6895a4cf3b136acddd2a02bd0fa9

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1577,9 +1577,7 @@ public class TelephonyManager {
             Rlog.d(TAG, "getSimState:- empty subId return SIM_STATE_ABSENT");
             return SIM_STATE_UNKNOWN;
         }
-        int simState = SubscriptionManager.getSimStateForSubscriber(subId[0]);
-        Rlog.d(TAG, "getSimState: simState=" + simState + " slotIdx=" + slotIdx);
-        return simState;
+        return SubscriptionManager.getSimStateForSubscriber(subId[0]);
     }
 
     /**
@@ -1629,7 +1627,6 @@ public class TelephonyManager {
                 }
             }
         }
-        Rlog.d(TAG, "getSimOperatorNumeric(): default subId=" + subId);
         return getSimOperatorNumericForSubscription(subId);
     }
 
