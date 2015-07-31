0642687c95c43b167481e00582b358a669d0dbfc
Shammi Khattar <skhattar@codeaurora.org>
2015-07-31 09:52:31 -0700

    Blank area appears between QS and Notification
    The blank area appears when users are switched between Owner
    and any other user while interacting with QS Tiles like the
    Cast Screen that results in height of the QS panel to change.
    
    Change-Id: I37bc7847618009eee90981b1dd4690e3fcfb78a0
    CRs-Fixed: 866559

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -359,6 +359,9 @@ public class NotificationPanelView extends PanelView implements
         updateHeader();
         mNotificationStackScroller.updateIsSmallScreen(
                 mHeader.getCollapsedHeight() + mQsPeekHeight);
+        if (mQsSizeChangeAnimator == null) {
+            mQsContainer.setHeightOverride(mQsContainer.getDesiredHeight());
+        }
 
         // If we are running a size change animation, the animation takes care of the height of
         // the container. However, if we are not animating, we always need to make the QS container
