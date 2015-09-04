7ae904f41875687f177808b588161ed680fe06ef
Steve Kondik <steve@cyngn.com>
2015-09-04 04:19:50 -0700

    power: Boost improvements
     * Boost when opening the panelview
     * Adjust launch boost to handle relaunch correctly
    
    Change-Id: I1cf4c07a8f4465bf718a4e7a882b44534c1ded65

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -2536,6 +2536,12 @@ public final class ActivityStackSupervisor implements DisplayListener {
     }
 
     void findTaskToMoveToFrontLocked(TaskRecord task, int flags, Bundle options, String reason) {
+        ActivityRecord top = task.stack.topRunningActivityLocked(null);
+        /* App is launching from recent apps and it's a new process */
+        if(top != null && top.state == ActivityState.DESTROYED) {
+            mPm.launchBoost();
+        }
+
         if ((flags & ActivityManager.MOVE_TASK_NO_USER_ACTION) == 0) {
             mUserLeaving = true;
         }
@@ -2729,7 +2735,9 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
                 final ActivityRecord ar = stack.findTaskLocked(r);
                 if (ar != null) {
-                    BinderInternal.modifyDelayedGcParams();
+                    if (ar.state == ActivityState.DESTROYED) {
+                        mPm.launchBoost();
+                    }
                     return ar;
                 }
             }
