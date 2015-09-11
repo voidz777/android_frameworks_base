150e916b5b6c3a68dfe9c6a8cd0ae8637f5e4eb9
d34d <clark@cyngn.com>
2015-09-11 10:17:19 -0700

    Revert "Notifications: Only check for spam if new"
    This reverts commit da2b45ae6104dec9d5c807e8af4e250cd31b2b59.
    
    Now that we are no longer hitting a content provider every time
    a package is not found in mSpamCache we can get rid of this change
    as it does cause problems since notifications that are marked as
    spam are not "removed" from the NotificationManagerService, they
    are just removed from the notification panel in SystemUI.
    
    Change-Id: I4743196fc26b4e9b4eb05512fe73ce1dc2e830e0

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2151,8 +2151,7 @@ public class NotificationManagerService extends SystemService {
                         return;
                     }
 
-                    // Only check for spam if this is a new notification
-                    if (old == null && isNotificationSpam(notification, pkg)) {
+                    if (isNotificationSpam(notification, pkg)) {
                         mArchive.record(r.sbn);
                         return;
                     }
