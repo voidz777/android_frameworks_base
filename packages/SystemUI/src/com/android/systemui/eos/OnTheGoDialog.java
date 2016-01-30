AICP/frameworks_base

Squashed On-The-Go mode (1/2)
On-The-Go turns on the camera of your phone and streams it to an overlay,
which is drawn over everything.
this makes you feel like your display is transparent, as you see the content
behind it.

its really useful when you are using your phone while walking, thats why
we called it "On-The-Go"

allows to change transparency of the overlay via the notification.

have fun!

To-Do

Add QS OnTheGo

packages/SystemUI/src/com/android/systemui/qs/tiles/OnTheGoTile.java

Screw'd: HEAVILY modified for Screw'd
-Modified for Screw'd PowerMenu style

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

AICPfy:
Picked from here and adapted:
https://github.com/MrApocalypse/frameworks_base/commit/45a242a370d806a5e5997fd7ecc800eac5e82427
Use our PowerMenu style implementation

Change-Id: I5800a2010a20215053004a2605e65503ae8dd5a1

packages/SystemUI/src/com/android/systemui/aicp/onthego/OnTheGoDialog.java
@@ -1,0 +1,176 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*    Copyright (C) 2015 AICP
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.systemui.aicp.onthego;
+
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import com.android.internal.util.aicp.AicpUtils;
+import com.android.systemui.R;
+
+public class OnTheGoDialog extends Dialog {
+
+    protected final Context mContext;
+    protected final Handler mHandler = new Handler();
+
+    private final int mOnTheGoDialogLongTimeout;
+    private final int mOnTheGoDialogShortTimeout;
+
+    private final Runnable mDismissDialogRunnable = new Runnable() {
+        public void run() {
+            if (OnTheGoDialog.this.isShowing()) {
+                OnTheGoDialog.this.dismiss();
+            }
+        }
+    };
+
+    public OnTheGoDialog(Context ctx) {
+        super(ctx);
+        mContext = ctx;
+        final Resources r = mContext.getResources();
+        mOnTheGoDialogLongTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_long_timeout);
+        mOnTheGoDialogShortTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_short_timeout);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
+        window.getAttributes().privateFlags |=
+                WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.quick_settings_onthego_dialog);
+        setCanceledOnTouchOutside(true);
+
+        final ContentResolver resolver = mContext.getContentResolver();
+
+        final SeekBar mSlider = (SeekBar) findViewById(R.id.alpha_slider);
+        final float value = Settings.System.getFloat(resolver,
+                Settings.System.ON_THE_GO_ALPHA,
+                0.5f);
+        final int progress = ((int) (value * 100));
+        mSlider.setProgress(progress);
+        mSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sendAlphaBroadcast(String.valueOf(i + 10));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                removeAllOnTheGoDialogCallbacks();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+
+        if (!AicpUtils.hasFrontCamera(getContext())) {
+            findViewById(R.id.onthego_category_1).setVisibility(View.GONE);
+        } else {
+            final Switch mServiceToggle = (Switch) findViewById(R.id.onthego_service_toggle);
+            final boolean restartService = Settings.System.getBoolean(resolver,
+                    Settings.System.ON_THE_GO_SERVICE_RESTART,
+                    false);
+            mServiceToggle.setChecked(restartService);
+            mServiceToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putBoolean(resolver,
+                            Settings.System.ON_THE_GO_SERVICE_RESTART,
+                            b);
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+
+            final Switch mCamSwitch = (Switch) findViewById(R.id.onthego_camera_toggle);
+            final boolean useFrontCam = (Settings.System.getInt(resolver,
+                    Settings.System.ON_THE_GO_CAMERA,
+                    0) == 1);
+            mCamSwitch.setChecked(useFrontCam);
+            mCamSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putInt(resolver,
+                            Settings.System.ON_THE_GO_CAMERA,
+                            (b ? 1 : 0));
+                    sendCameraBroadcast();
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dismissOnTheGoDialog(mOnTheGoDialogLongTimeout);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        removeAllOnTheGoDialogCallbacks();
+    }
+
+    private void dismissOnTheGoDialog(int timeout) {
+        removeAllOnTheGoDialogCallbacks();
+        mHandler.postDelayed(mDismissDialogRunnable, timeout);
+    }
+
+    private void removeAllOnTheGoDialogCallbacks() {
+        mHandler.removeCallbacks(mDismissDialogRunnable);
+    }
+
+    private void sendAlphaBroadcast(String i) {
+        final float value = (Float.parseFloat(i) / 100);
+        final Intent alphaBroadcast = new Intent();
+        alphaBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_ALPHA);
+        alphaBroadcast.putExtra(OnTheGoService.EXTRA_ALPHA, value);
+        mContext.sendBroadcast(alphaBroadcast);
+    }
+
+    private void sendCameraBroadcast() {
+        final Intent cameraBroadcast = new Intent();
+        cameraBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_CAMERA);
+        mContext.sendBroadcast(cameraBroadcast);
+    }
+
+}

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -104,6 +104,8 @@ import java.util.UUID;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
 
+import com.android.internal.util.aicp.AicpActions;
+
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
  * may show depending on whether the keyguard is showing, and whether the device
@@ -347,6 +349,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mItems.add(getLockdownAction());
             } else if (GLOBAL_ACTION_KEY_TORCH.equals(actionKey)) {
                 mItems.add(getTorchToggleAction());
+            } else if (GLOBAL_ACTION_KEY_ONTHEGO.equals(actionKey)) {
+                mItems.add(getOTGToggleAction());
             } else {
                 Log.e(TAG, "Invalid global action key " + actionKey);
             }
@@ -511,6 +515,31 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
         };
     }
+	
+    private Action getOTGToggleAction() {
+	return new SinglePressAction(com.android.internal.R.drawable.ic_lock_onthego,
+        		R.string.global_action_onthego) {
+
+           public void onPress() {
+            	AicpActions.processAction(mContext,
+                 AicpActions.ACTION_ONTHEGO_TOGGLE);
+		return;	
+            }
+
+            public boolean onLongPress() {
+            	return false;
+            }
+
+            public boolean showDuringKeyguard() {
+            	return true;
+            }
+
+            public boolean showBeforeProvisioning() {
+            	return true;
+            }
+
+        };
+     }
 
     private Action getBugReportAction() {
         return new SinglePressAction(com.android.internal.R.drawable.ic_lock_bugreport,
@@ -825,6 +854,15 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mHandler.postDelayed(mScreenrecordTimeout, 31 * 60 * 1000);
             }
         }
+    }
+
+    private void startOnTheGo() {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.aicp.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        mContext.startService(startIntent);
     }
 
     private void prepareDialog() {

voidz777/android_frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	core/java/com/android/internal/util/aicp/AicpUtils.java
	core/java/com/android/internal/util/cm/PowerMenuConstants.java
	core/java/com/android/internal/util/cm/QSConstants.java
	core/res/res/values/config.xml
	core/res/res/values/strings_aicp.xml
	core/res/res/values/symbols_aicp.xml
	packages/SystemUI/res/values/custom_config.xml
	packages/SystemUI/res/values/strings_aicp.xml
	policy/src/com/android/internal/policy/impl/GlobalActions.java
	core/res/res/values/custom_symbols.xml

packages/SystemUI/src/com/android/systemui/eos/OnTheGoDialog.java
@@ -1,0 +1,176 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*    Copyright (C) 2015 AICP
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.systemui.eos.onthego;
+
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+
+import com.android.internal.util.eos.EOSUtils;
+import com.android.systemui.R;
+
+public class OnTheGoDialog extends Dialog {
+
+    protected final Context mContext;
+    protected final Handler mHandler = new Handler();
+
+    private final int mOnTheGoDialogLongTimeout;
+    private final int mOnTheGoDialogShortTimeout;
+
+    private final Runnable mDismissDialogRunnable = new Runnable() {
+        public void run() {
+            if (OnTheGoDialog.this.isShowing()) {
+                OnTheGoDialog.this.dismiss();
+            }
+        }
+    };
+
+    public OnTheGoDialog(Context ctx) {
+        super(ctx);
+        mContext = ctx;
+        final Resources r = mContext.getResources();
+        mOnTheGoDialogLongTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_long_timeout);
+        mOnTheGoDialogShortTimeout =
+                r.getInteger(R.integer.quick_settings_onthego_dialog_short_timeout);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setType(WindowManager.LayoutParams.TYPE_VOLUME_OVERLAY);
+        window.getAttributes().privateFlags |=
+                WindowManager.LayoutParams.PRIVATE_FLAG_SHOW_FOR_ALL_USERS;
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.quick_settings_onthego_dialog);
+        setCanceledOnTouchOutside(true);
+
+        final ContentResolver resolver = mContext.getContentResolver();
+
+        final SeekBar mSlider = (SeekBar) findViewById(R.id.alpha_slider);
+        final float value = Settings.System.getFloat(resolver,
+                Settings.System.ON_THE_GO_ALPHA,
+                0.5f);
+        final int progress = ((int) (value * 100));
+        mSlider.setProgress(progress);
+        mSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                sendAlphaBroadcast(String.valueOf(i + 10));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                removeAllOnTheGoDialogCallbacks();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+            }
+        });
+
+        if (!EOSUtils.hasFrontCamera(getContext())) {
+            findViewById(R.id.onthego_category_1).setVisibility(View.GONE);
+        } else {
+            final Switch mServiceToggle = (Switch) findViewById(R.id.onthego_service_toggle);
+            final boolean restartService = Settings.System.getBoolean(resolver,
+                    Settings.System.ON_THE_GO_SERVICE_RESTART,
+                    false);
+            mServiceToggle.setChecked(restartService);
+            mServiceToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putBoolean(resolver,
+                            Settings.System.ON_THE_GO_SERVICE_RESTART,
+                            b);
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+
+            final Switch mCamSwitch = (Switch) findViewById(R.id.onthego_camera_toggle);
+            final boolean useFrontCam = (Settings.System.getInt(resolver,
+                    Settings.System.ON_THE_GO_CAMERA,
+                    0) == 1);
+            mCamSwitch.setChecked(useFrontCam);
+            mCamSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    Settings.System.putInt(resolver,
+                            Settings.System.ON_THE_GO_CAMERA,
+                            (b ? 1 : 0));
+                    sendCameraBroadcast();
+                    dismissOnTheGoDialog(mOnTheGoDialogShortTimeout);
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dismissOnTheGoDialog(mOnTheGoDialogLongTimeout);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        removeAllOnTheGoDialogCallbacks();
+    }
+
+    private void dismissOnTheGoDialog(int timeout) {
+        removeAllOnTheGoDialogCallbacks();
+        mHandler.postDelayed(mDismissDialogRunnable, timeout);
+    }
+
+    private void removeAllOnTheGoDialogCallbacks() {
+        mHandler.removeCallbacks(mDismissDialogRunnable);
+    }
+
+    private void sendAlphaBroadcast(String i) {
+        final float value = (Float.parseFloat(i) / 100);
+        final Intent alphaBroadcast = new Intent();
+        alphaBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_ALPHA);
+        alphaBroadcast.putExtra(OnTheGoService.EXTRA_ALPHA, value);
+        mContext.sendBroadcast(alphaBroadcast);
+    }
+
+    private void sendCameraBroadcast() {
+        final Intent cameraBroadcast = new Intent();
+        cameraBroadcast.setAction(OnTheGoService.ACTION_TOGGLE_CAMERA);
+        mContext.sendBroadcast(cameraBroadcast);
+    }
+
+}

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -103,6 +103,8 @@ import java.util.UUID;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
 
+import com.android.internal.util.eos.EOSActions;
+
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
  * may show depending on whether the keyguard is showing, and whether the device
@@ -337,6 +339,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mItems.add(getSettingsAction());
             } else if (GLOBAL_ACTION_KEY_LOCKDOWN.equals(actionKey)) {
                 mItems.add(getLockdownAction());
+            } else if (GLOBAL_ACTION_KEY_ONTHEGO.equals(actionKey)) {
+                mItems.add(getOTGToggleAction());
             } else {
                 Log.e(TAG, "Invalid global action key " + actionKey);
             }
@@ -468,6 +472,31 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
         };
     }
+	
+    private Action getOTGToggleAction() {
+	return new SinglePressAction(com.android.internal.R.drawable.ic_lock_onthego,
+        		R.string.global_action_onthego) {
+
+           public void onPress() {
+            	EOSActions.processAction(mContext,
+                 EOSActions.ACTION_ONTHEGO_TOGGLE);
+		return;	
+            }
+
+            public boolean onLongPress() {
+            	return false;
+            }
+
+            public boolean showDuringKeyguard() {
+            	return true;
+            }
+
+            public boolean showBeforeProvisioning() {
+            	return true;
+            }
+
+        };
+     }
 
     private Action getBugReportAction() {
         return new SinglePressAction(com.android.internal.R.drawable.ic_lock_bugreport,
@@ -722,6 +751,15 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private void toggleScreenRecord() {
         final Intent recordIntent = new Intent("org.chameleonos.action.NOTIFY_RECORD_SERVICE");
         mContext.sendBroadcast(recordIntent, Manifest.permission.RECORD_SCREEN);
+    }
+
+    private void startOnTheGo() {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.eos.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        mContext.startService(startIntent);
     }
 
     private void prepareDialog() {